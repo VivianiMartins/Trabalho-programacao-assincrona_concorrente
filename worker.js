8cd245da630efbc5bd1f6a37e2f9dcf312792ac7
viviani <vivianimartinssantos@gmail.com>
2023-12-06 09:23:27 -0300

    Ajustes finais para apresentação

--- worker.js
+++ worker.js
@@ -3,20 +3,17 @@ var resultado = [];
 self.onmessage = (array) => {
     let elementos = array.data[0];
     let ultimaPosicao = 0;
-    const outro = [];
-    let country = array.data[2];
-    let tamanho = elementos.length;
-
+    //const outro = data[1];
     for(let i= 0; i < tamanho; i++){
         //console.log(elementos[i][1]);
         if(elementos[i][1] == country){
-            outro[ultimaPosicao] =  elementos[i][5];
+            Atomics.add(array.data[1], ultimaPosicao, elementos[i][5]);
+            //outro[ultimaPosicao] =  elementos[i][5];
             ultimaPosicao++;
         }
     }
-    console.log("Array com a lista de resultados");
-    console.log(outro);
-    pegarResultado(outro);
+    //console.log("Array com a lista de resultados");
+    //console.log(outro);
 };
 //para tentar enviar para o index.js para exibir
 function pegarResultado(outro){
