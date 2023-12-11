4f5a9f9a4a568e600a972577e31f0450c1bb11f9
viviani <vivianimartinssantos@gmail.com>
2023-12-11 10:17:54 -0300

    Alterações para que possamos usar os workers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-
 let country = '';
-let datatablePesquisa = document.querySelector("#table_search_result");
-let paginaAtual = 0;
-let pesquisaAtual = '';
 let result = document.querySelector('#result');
 
 window.getStarted = function () {
@@ -14,9 +10,11 @@ window.getStarted = function () {
         alert("Vamos começar!");
         if (window.Worker) {
             console.log('entrou');
-            const myWorker = new Worker("./workers/collectDataWorker.mjs", {type: 'module'});
-            myWorker.postMessage(countryValue);
+            let datatablePesquisa = $("#table_search_result");
+            let paginaAtual = 0;
+            let pesquisaAtual = '';
 
+            const myWorker = new Worker("./workers/collectDataWorker.mjs", {type: 'module'});
             datatablePesquisa.DataTable({
                 paging: false,
                 dom: 'Bfrtip',
@@ -43,37 +41,37 @@ window.getStarted = function () {
                 ]
             });
 
+            function alteraPagina(pagina, pesquisa) {
+                pesquisaAtual === pesquisa ? paginaAtual += pagina : paginaAtual = 1;
+                pesquisaAtual = pesquisa;
+            }
+            async function preencherTabelaPesquisa(city = '', page = 1, limit = 10) {
+                datatablePesquisa.DataTable().rows().remove();
+                alteraPagina(page, city);
+                let url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?languageCode=pt_BR&limit=10&sort=name';
+
+                var cities =  fazRequisicao(url);
+
+                if (cities) {
+                    alert('Nenhuma cidade foi encontrado!');
+                } else {
+                    preencherTabelaRec(cities);
+                }
+            }
+            function preencherTabelaRec(cities=[]) {
+                myWorker.onmessage = function(e) {
+                    result.textContent = e.data;
+                    console.log('Recebeu retorno dos workers!');
+                }
+                let tamanho = cities.length;
+                console.log("Tamanho para preencher, %i", tamanho);
+                datatablePesquisa.DataTable().row.add([cities["id"], cities["population"], 10]).draw();
+            }
+
         } else {
             console.log('Seu browser não suporta web workers.');
         }
     } else {
         alert("Favor inserir nome do país");
     }
-}
-
-function alteraPagina(pagina, pesquisa) {
-    pesquisaAtual === pesquisa ? paginaAtual += pagina : paginaAtual = 1;
-    pesquisaAtual = pesquisa;
-}
-async function preencherTabelaPesquisa(city = '', page = 1, limit = 10) {
-    datatablePesquisa.DataTable().rows().remove();
-    alteraPagina(page, city);
-    let url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?languageCode=pt_BR&limit=10&sort=name';
-
-    var cities =  fazRequisicao(url);
-
-    if (cities) {
-        alert('Nenhuma cidade foi encontrado!');
-    } else {
-        preencherTabelaRec(cities);
-    }
-}
-function preencherTabelaRec(cities=[]) {
-    myWorker.onmessage = function(e) {
-        result.textContent = e.data;
-        console.log('Recebeu retorno dos workers!');
-    }
-    let tamanho = cities.length;
-    console.log("Tamanho para preencher, %i", tamanho);
-    datatablePesquisa.DataTable().row.add([cities["id"], cities["population"], 10]).draw();
 }
--- src/server.js
+++ src/server.js
@@ -5,12 +5,27 @@ const app = express();
 //usando as rotas criadas
 const routes = require('./routes');
 const path = require('path');
+
+app.use(function(req, res, next) {
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+});
+
 app.use((req, res, next) => {
     if (path.extname(req.url) === '.mjs') {
         res.type('application/javascript');
     }
     next();
 });
+
+app.use((req, res, next) => {
+    if (path.extname(req.url) === '.css') {
+        res.type('text/css');
+    }
+    next();
+});
+
 app.use(express.json());
 app.use('/', routes);
 //iniciando servidor na porta 3080
