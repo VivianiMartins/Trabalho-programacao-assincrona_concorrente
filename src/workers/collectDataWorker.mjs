39834ece02bff997cc59798154298fe9e9f585ea
viviani <vivianimartinssantos@gmail.com>
2023-12-12 10:13:27 -0300

    Alterações para coletar os dados através de workers

--- src/workers/collectDataWorker.mjs
+++ src/workers/collectDataWorker.mjs
@@ -1,6 +1,10 @@
-const key0 = { apiKey: 'dd9d5ded5cmsha4eaedebadf7b68p173c21jsn10c725023f5b'};
+//v2
+const key0 = { apiKey: 'af27a3e0abmsha4afad9c0ff7f47p19c5f0jsn71ce6955627c'};
+//l1
 const key1 = { apiKey: 'dd9d5ded5cmsha4eaedebadf7b68p173c21jsn10c725023f5b'};
+
 const key2 = { apiKey: 'dd9d5ded5cmsha4eaedebadf7b68p173c21jsn10c725023f5b'};
+//v1
 const key3 = { apiKey: 'dd9d5ded5cmsha4eaedebadf7b68p173c21jsn10c725023f5b'};
 
 const numeroWorkers = 4;
--- src/workers/dataWorker.mjs
+++ src/workers/dataWorker.mjs
@@ -1,60 +1,44 @@
 const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?languageCode=pt_BR&limit=10&sort=name';
-
-let cabecalhoRequisicao = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': ' ',
-        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    }
-};
+let chaveVariavel = 'chaveTemporária';
 
 onmessage = function (array) {
     //Separando o buffer e o cabeçalho
     console.log('O que estou recebendo:', array);
-    let bufferCompartilhado = array.data[0];
-    let chave = array.data[1];
-
-    console.log('Buffer Compartilhado:', bufferCompartilhado);
-    console.log('Chave:', chave);
-    bufferCompartilhado = fazRequisicao(url);
-
+    let bufferCompartilhado = array.data.buffer;
+    chaveVariavel = array.data.key.apiKey;
+    
+    fazRequisicao(url, bufferCompartilhado);
+    
 };
 
-
-
-for(let i = 0; i < arrayCity.length; i++){
-    arrayCity[i][0] = arrayCity[i]['city'];
-    arrayCity[i][1] = arrayCity[i]['country'];
-    arrayCity[i][2] = arrayCity[i]['latitude'];
-    arrayCity[i][3] = arrayCity[i]['longitude'];
-    if(arrayCity[i]['population']>1){
-        arrayCity[i][4] = arrayCity[i]['population'];
-    } else {
-        arrayCity[i][4] = 1.0001;
-    };
-    console.log(arrayCity[i]);
-
-}
-
-
-async function fazRequisicao(url){
-    var arrayCity = [];
-    var arrayCities = await realizaRequisicao(url, cabecalhoRequisicao);
-
-    //pegado as 10 primeiras cidades
-    for (let i = 0; i < 10; i++) {
-        arrayCity[i] = arrayCities.data[i];
+let cabecalhoRequisicao = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': chaveVariavel,
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
-    //tentando pegar pagina por pagina p construir o array de respostas
-    coletarDados(arrayCity, 10, 10, 5000);
-    console.log("foi um");
-    console.log(arrayCity.length);
+};
 
+async function fazRequisicao(url, bufferCompartilhado){
+    const resultadoRequisicao = await realizaRequisicao(url, cabecalhoRequisicao);
 
+    //pegado as 10 primeiras cidades e colocando no array
+    for (let i = 0; i < 10; i++) {
+        // Armazene os dados no objeto bufferCompartilhado
+        bufferCompartilhado.data = resultadoRequisicao.data[i];
+    }
 
-    return  arrayCity;
+    //Colocando o restante das cidades no array, tempo sendo aumentado para não haver problemas de requisição/
+    //pelos testes não pudemos colocar intervalo menor de 1,5 segundos entre cada
+    let tempo = 1500;
+    for(let j = 10; j < 2000; j = j + 10){
+        await coletarDados(bufferCompartilhado, j, 10, tempo);
+        tempo = tempo + 1500;
+    }
+
+    // Retorne o objeto bufferCompartilhado
+    postMessage(bufferCompartilhado);
 }
-
 
 async function realizaRequisicao(url, cabecalhoRequisicao) {
     try {
@@ -63,19 +47,26 @@ async function realizaRequisicao(url, cabecalhoRequisicao) {
     } catch (error) {
         console.error(error);
     }
+    return bufferCompartilhado;
 }
 
-function coletarDados(arrayCity, min, max, time){
+function coletarDados(bufferCompartilhado, min, max, time){
     setTimeout(async () => {
         let urlTemp = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?offset=${min}&limit=${max}&languageCode=pt_BR&sort=name`;
         var tempArray = await realizaRequisicao(urlTemp, cabecalhoRequisicao);
 
-        let j = arrayCity.length;
+        let j = bufferCompartilhado.length;
         console.log(j);
         for (let i = 0; i < 10; i++) {
-            arrayCity.push(tempArray.data[i]);
+            bufferCompartilhado.data.push(
+                tempArray.data[i][0] = tempArray.data[i]['city'],
+                tempArray.data[i][1] = tempArray.data[i]['country'],
+                tempArray.data[i][2] = tempArray.data[i]['latitude'],
+                tempArray.data[i][3] = tempArray.data[i]['longitude'],
+                tempArray.data[i][4] = tempArray.data[i]['population']
+            );
             j++;
         }
-        return arrayCity;
+        return bufferCompartilhado;
     }, time);
 }
