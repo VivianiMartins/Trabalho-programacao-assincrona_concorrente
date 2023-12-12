8ba91855878645e268e54519e87aa652c6a19bc9
viviani <vivianimartinssantos@gmail.com>
2023-12-12 17:59:09 -0300

    Alterações para coletar os dados através de workers, a coleta está funcionando mas não consigo armazenar nos buffers

--- src/workers/dataWorker.mjs
+++ src/workers/dataWorker.mjs
@@ -1,39 +1,58 @@
 const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?languageCode=pt_BR&limit=10&sort=name';
-let chaveVariavel = 'chaveTemporária';
+let chaveVariavel = '';
+let tempo = 0;
 
 onmessage = async function (array) {
     //Separando o buffer e o cabeçalho
     let bufferCompartilhado = array.data.buffer;
+    let bufferView = new Int32Array(bufferCompartilhado);
     chaveVariavel = array.data.key.apiKey;
-    await fazRequisicao(url, bufferCompartilhado);
+    await fazRequisicao(url, bufferView);
 };
 
-async function fazRequisicao(url, bufferCompartilhado){
+async function fazRequisicao(url, bufferView){
+    let cabecalhoRequisicao = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': chaveVariavel,
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+    };
     //Colocando o restante das cidades no array, tempo sendo aumentado para não haver problemas de requisição
     //pelos testes não pudemos colocar intervalo menor de 1,5 segundos entre cada
     //primeira requisição de cada:
-    var tempArray = await realizaRequisicao(url, cabecalhoRequisicao);
+    let tempArray = await realizaRequisicao(url, cabecalhoRequisicao);
+    console.log('temp com os 10 primeiros', tempArray);
+
+    for (let i = 0; i < 10; i++) {
+        tempArray.data[i] = [
+            tempArray.data[i]['city'],
+            tempArray.data[i]['country'],
+            tempArray.data[i]['latitude'],
+            tempArray.data[i]['longitude'],
+            tempArray.data[i]['population']
+        ];
+    }
     for (let i = 0; i < 10; i++) {
         // Armazene os dados no objeto bufferCompartilhado
-        Atomics.store(bufferCompartilhado, i, tempArray.data[i]);
+        Atomics.store(bufferView, i, tempArray.data[i]);
     }
+    console.log('buffer depois dos 10 primeiros', bufferView);
 
-    let tempo = 1500;
-    await sleep(tempo);
-    console.log(tempo);
-
-    for(let j = 10; j < 500; j = j + 10){
+    tempo = 1500;
+    for(let j = 10; j < 40; j = j + 10){
         //ainda tenho que incrementar o tempo, para fazer mais requisições em cada worker
         tempo = tempo + 1500;
-        tempArray = await coletarDados( bufferCompartilhado, j, 10, tempo);
-        console.log(tempArray);
+        tempArray = await coletarDados( bufferView, j, 10, tempo, cabecalhoRequisicao);
+        console.log('temp com retorno da coleta', tempArray)
         for (let i = 0; i < 10; i++) {
             // Armazene os dados no objeto bufferCompartilhado
-            Atomics.store(bufferCompartilhado, i, tempArray.data[i]);
+            Atomics.store(bufferView, i, tempArray.data[i]);
         }
+        console.log('buffer depois da coleta', bufferView);
     }
     // Retorne o objeto bufferCompartilhado
-    postMessage({ status: 'concluido', buffer: bufferCompartilhado });
+    postMessage( bufferView);
 }
 
 async function realizaRequisicao(url, cabecalhoRequisicao) {
@@ -42,35 +61,26 @@ async function realizaRequisicao(url, cabecalhoRequisicao) {
         return response.json();
     } catch (error) {
         console.error(error);
+        //retorno vazio
+        return { data: [] };
     }
-    return bufferCompartilhado;
 }
 
-let cabecalhoRequisicao = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': chaveVariavel,
-        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    }
-};
-function coletarDados( bufferCompartilhado, min, max, tempo){
+function coletarDados( bufferView, min, max, tempo, cabecalhoRequisicao){
     return new Promise(async (resolve) => {
         await sleep(tempo);
-        console.log(tempo);
         let urlTemp = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?offset=${min}&limit=${max}&languageCode=pt_BR&sort=name`;
         var tempArray = await realizaRequisicao(urlTemp, cabecalhoRequisicao);
-        let j = bufferCompartilhado.length;
-        console.log(j);
         for (let i = 0; i < 10; i++) {
-            tempArray.data.push(
-                tempArray.data[i][0] = tempArray.data[i]['city'],
-                tempArray.data[i][1] = tempArray.data[i]['country'],
-                tempArray.data[i][2] = tempArray.data[i]['latitude'],
-                tempArray.data[i][3] = tempArray.data[i]['longitude'],
-                tempArray.data[i][4] = tempArray.data[i]['population']
-            );
-            j++;
+            tempArray.data[i] = [
+                tempArray.data[i]['city'],
+                tempArray.data[i]['country'],
+                tempArray.data[i]['latitude'],
+                tempArray.data[i]['longitude'],
+                tempArray.data[i]['population']
+            ];
         }
+        console.log('temp array coleta dados', tempArray);
         //para esperar mais
         resolve(tempArray);
     });
